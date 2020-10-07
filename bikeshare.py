6ea5b29bd46e6b29b711945174648cfa4f6f9ebb
Abdel23000 <fa_azieze@esi.dz>
2020-10-07 15:14:48 +0100

    Add comment to code file

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,8 @@ import math
 import pandas as pd
 import numpy as np
 
+######################################################################################## Date data ########################################################################################
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
